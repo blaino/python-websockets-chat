569947c026bf5d0ab88a536cbaf85390da36ac0e
Blaine Nelson <blaine.nelson@bts-s2.com>
2014-03-11 18:07:11 -0400

    Working, but still some gevent stuff going.

--- chat.py
+++ chat.py
@@ -9,19 +9,14 @@ This simple application uses WebSockets to run a primitive chat server.
 
 import os
 import logging
-import redis
 import gevent
 from flask import Flask, render_template
 from flask_sockets import Sockets
-
-REDIS_URL = os.environ['REDISCLOUD_URL']
-REDIS_CHAN = 'chat'
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
 
 sockets = Sockets(app)
-redis = redis.from_url(REDIS_URL)
 
 
 class ChatBackend(object):
@@ -29,40 +24,17 @@ class ChatBackend(object):
 
     def __init__(self):
         self.clients = list()
-        self.pubsub = redis.pubsub()
-        self.pubsub.subscribe(REDIS_CHAN)
 
-    def __iter_data(self):
-        for message in self.pubsub.listen():
-            data = message.get('data')
-            if message['type'] == 'message':
-                app.logger.info(u'Sending message: {}'.format(data))
-                yield data
+    def publish(self, message):
+        for client in self.clients:
+            print message
+            client.send(message)
 
     def register(self, client):
-        """Register a WebSocket connection for Redis updates."""
+        """Register a WebSocket connection."""
         self.clients.append(client)
 
-    def send(self, client, data):
-        """Send given data to the registered client.
-        Automatically discards invalid connections."""
-        try:
-            client.send(data)
-        except Exception:
-            self.clients.remove(client)
-
-    def run(self):
-        """Listens for new messages in Redis, and sends them to clients."""
-        for data in self.__iter_data():
-            for client in self.clients:
-                gevent.spawn(self.send, client, data)
-
-    def start(self):
-        """Maintains Redis subscription in the background."""
-        gevent.spawn(self.run)
-
 chats = ChatBackend()
-chats.start()
 
 
 @app.route('/')
@@ -74,13 +46,13 @@ def hello():
 def inbox(ws):
     """Receives incoming chat messages, inserts them into Redis."""
     while ws.socket is not None:
-        # Sleep to prevent *contstant* context-switches.
+        # # Sleep to prevent *contstant* context-switches.
         gevent.sleep(0.1)
         message = ws.receive()
 
         if message:
             app.logger.info(u'Inserting message: {}'.format(message))
-            redis.publish(REDIS_CHAN, message)
+            chats.publish(message)
 
 
 @sockets.route('/receive')
--- chatroom.py
+++ chatroom.py
@@ -2,14 +2,22 @@ class Chatroom:
 
     def __init__(self, room_name):
         self.room_name = room_name
+        self.messages = list()
         self.message_observers = []
 
     def publish(self, message):
+        self.messages.append(message)
         for observer in self.message_observers:
             observer.chatroom_callback(message, self.room_name)
 
     def subscribe(self, observer):
+        print "Subscribed: "
+        print observer
         self.message_observers.append(observer)
+
+    def listen(self):
+        for message in self.messages:
+            yield message
 
 
 class ChatroomObserver:
@@ -22,14 +30,18 @@ class ChatroomObserver:
         print "The message says: " + message
 
 
-
 # >>> from observer import *
+
+# >>> cr1 = Chatroom("ch1")
+
 # >>> alice = ChatroomObserver("alice")
 # >>> bob = ChatroomObserver("bob")
-# >>> cr1 = Chatroom("ch1")
+
 # >>> cr1.subscribe(bob)
 # >>> cr1.subscribe(alice)
+
 # >>> cr1.publish("blik")
+
 # bob observed a new message in ch1
 # The message says: blik
 # alice observed a new message in ch1
